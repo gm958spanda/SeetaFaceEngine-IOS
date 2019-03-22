fcb9f7139f8133b3eb62ef1934b1316d9c6979b6
zxs.zl <zhangxiangsheng@mskj.com>
2019-03-22 13:11:15 +0800

    IOS-Demo工程

---
+++ SeetaFace-IOS/SeetaFace-IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeetaFace-IOS
+//
+//  Created by zxs.zl on 2019/3/22.
+//  Copyright © 2019 zxs.zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeetaFace-IOS/SeetaFace-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeetaFace-IOS
+//
+//  Created by zxs.zl on 2019/3/22.
+//  Copyright © 2019 zxs.zl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
